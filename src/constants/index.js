da6716f6939a4f062dd168d68e883471d2b45c86
Richter Lau <ar.rick@hotmail.com>
2023-12-08 23:54:08 -0500

    adding experiences

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+const experiences = [
+  {
+    title: 'Software Engineer',
+    company_name: 'IMAGINEXT',
+    icon: starbucks,
+    iconBg: '#383E56',
+    date: '2022 - Current',
+    points: [
+      'Leveraged the Next.js framework and Node.js runtime environment to seamlessly integrate file system and API functionalities, resulting in an interactive and efficient debugging experience for developers.',
+      'Published package to npm registry, enabling seamless installation and empowering developers to incorporate and leverage the package in their projects, fostering collaboration and expanding the reach of the development community.',
+      'Integrated TypeScript, leveraging static typing for scalability, smoother onboarding of new developers, easier dependency upgrades, and streamlined codebase maintenance, enhancing overall development efficiency and future-proofing.',
+      'Implemented abstract syntax tree parser to accurately identify the variable types, and location of each variable declaration and enable features such as data rendering type identification, props passing, and API usage of the application.',
+      'Integrated D3 to generate a visual representation of the path hierarchy of the application, making it simpler for developers to understand and navigate complex Next.js applications by providing visibility to path relationships.',
+      'Crafted complex E2E tests using Cypress to ensure the robustness and reliability of the application, saving significant time by automating the testing process and facilitating rapid identification and resolution of potential issues.',
+      'Product developed under tech accelerator with OS Labs (opensourcelabs.io)',
+    ],
+  },
+  {
+    title: 'Market Risk Associate',
+    company_name: 'CHINA MERCHANTS BANK',
+    icon: starbucks,
+    iconBg: '#383E56',
+    date: '2016 - Current',
+    points: [
+      'Developed 100% of the fair market value models and seamlessly integrated them with third-party APIs to provide accurate and reliable pricing data, facilitating efficient decision-making and analysis across various domains.',
+      'Optimized monitoring tools for auditing and enforcing internal policies for foreign exchange rates using VBA and custom algorithms, achieving an 80% reduction in processing time while ensuring compliance with internal profit and loss limits.',
+      'Automated file transfer processes and folder creation tasks through Batch Script, resulting in a productivity increase of over one hour per week.',
+      'Leveraged SQL databases to design and maintain monthly transaction reports, ensuring the generation of reliable summary statistics.',
+    ],
+  },
+];
