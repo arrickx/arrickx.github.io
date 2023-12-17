3884d38d891d70e4421059e1352e8605831ba44d
Richter Lau <ar.rick@hotmail.com>
2023-12-17 11:08:17 -0500

    adding under construction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import { motion } from 'framer-motion';
 import './index.css';
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+    <motion.div 
+      className='min-h-screen flex items-center justify-center bg-gray-100'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <div className='text-center'>
+        <motion.div 
+          className='flex justify-center'
+          initial={{ y: -250 }}
+          animate={{ y: -10 }}
+          transition={{ type: 'spring', stiffness: 120 }}
+        >
+          <img
+            className='rounded-full'
+            src='https://th.bing.com/th/id/OIG.fQPnFHafx91N7NpY3SfM?w=1024&h=1024&rs=1&pid=ImgDetMain'
+            width='500'
+            height='500'
+          />
+        </motion.div>
+        <motion.h1 
+          className='text-6xl font-bold text-gray-900 mb-4'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1 }}
+        >
+          Under Construction
+        </motion.h1>
+        <motion.p 
+          className='text-gray-700'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+        >
+          We&apos;re working hard to finish the development of this site. Our
+          target launch date is
+          <span className='font-bold text-gray-900'> January 2024</span>!
+        </motion.p>
+      </div>
+    </motion.div>
   );
 }
 
