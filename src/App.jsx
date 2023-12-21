46ca44b138afdafad8dc9efce23a9bb6da927d5f
Richter Lau <ar.rick@hotmail.com>
2023-12-20 21:33:37 -0500

    adding alt to img

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
     <div>
       <div className='font-outfit justify-end items-center bg-stone-950 flex flex-col px-20 py-12 max-md:px-5'>
         {/* hero */}
-        <header className='w-full max-w-[1440px] mt-14 max-md:max-w-full max-md:mt-10'>
+        <header className='w-full max-w-[1440px] mt-14 max-md:max-w-full max-md:mt-40 section-break'>
           <div className='gap-5 flex max-md:flex-col max-md:items-center max-md:gap-0'>
-            <article className='flex flex-col items-stretch w-[55%] mt-[25%] max-md:w-full max-md:ml-0 max-md:mt-8 max-md:order-2'>
+            <article className='flex flex-col items-stretch w-[55%] mt-[25%] max-md:w-full max-md:ml-0 max-md:mt-20 max-md:order-2'>
               <div className='justify-center items-stretch flex flex-col'>
                 <h1 className='text-white text-left font-bold text-5xl tracking-widest leading-[80px] max-xl:text-4xl max-md:text-3xl max-md:leading-10  max-md:text-center'>
                   RICHTER LAU
                 </h1>
-                <p className='text-zinc-400 text-left text-3xl leading-15 max-xl:text-2xl max-lg:text-xl max-md:text-center'>
+                <p className='text-zinc-400 text-left text-3xl leading-15 max-xl:text-2xl max-lg:text-xl max-md:text-center max-md:mt-6'>
                   In the code flow, building impossible.
                 </p>
               </div>
@@ -22,6 +22,7 @@ function App() {
               <img
                 loading='lazy'
                 src={img.hero}
+                alt='Hero Image'
                 className='w-full max-md:mt-50 rounded-[50px] '
               />
             </figure>
@@ -36,18 +37,18 @@ function App() {
           <section className=' max-md:order-2'>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.nodeLogo} />
+                <img loading='lazy' src={img.nodeLogo} alt='Node.js Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.mongoLogo} className='p-2.5' />
+                <img loading='lazy' src={img.mongoLogo} alt='MongoDB Logo' className='p-2.5' />
               </div>
             </div>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.psqlLogo} />
+                <img loading='lazy' src={img.psqlLogo} alt='PostgreSQL Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.graphqlLogo} />
+                <img loading='lazy' src={img.graphqlLogo} alt='GraphQL Logo' />
               </div>
             </div>
             <p className='text-subtitle icon-title'>back-end</p>
@@ -56,18 +57,18 @@ function App() {
           <section className='max-md:order-1'>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.reactLogo} />
+                <img loading='lazy' src={img.reactLogo} alt='React Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.jsLogo} />
+                <img loading='lazy' src={img.jsLogo} alt='JavaScript Logo' />
               </div>
             </div>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.reduxLogo} />
+                <img loading='lazy' src={img.reduxLogo} alt='Redux Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.tsLogo} />
+                <img loading='lazy' src={img.tsLogo} alt='TypeScript Logo' />
               </div>
             </div>
             <p className='text-subtitle icon-title'>front-end</p>
@@ -76,18 +77,18 @@ function App() {
           <section className='max-md:order-3'>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.openaiLogo} />
+                <img loading='lazy' src={img.openaiLogo} alt='OpenAI Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.figmaLogo} className='p-1.5' />
+                <img loading='lazy' src={img.figmaLogo} alt='Figma Logo' className='p-1.5' />
               </div>
             </div>
             <div className='icon-row'>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.gitLogo} />
+                <img loading='lazy' src={img.gitLogo} alt='Git Logo' />
               </div>
               <div className='icon-px bg-zinc-900'>
-                <img loading='lazy' src={img.jestLogo} />
+                <img loading='lazy' src={img.jestLogo} alt='Jest Logo' />
               </div>
             </div>
             <p className='text-subtitle icon-title'>testings & tools</p>
@@ -167,34 +168,49 @@ function App() {
         <section className='section-wrap'>
           <div className='section-column '>
             <p className='text-title2'>OpenAI Exploration</p>
-            <img loading='lazy' src={img.gptLogo} className='project-img' />
+            <a
+              href='https://github.com/arrickx/openai-exploration'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img loading='lazy' src={img.gptLogo} alt='GPT Logo' className='project-img' />
+            </a>
             <p className='project-desc'>
               This project demonstrates various functionalities of the OpenAI
               API,including chat, Document QA, function calling, and semantic
               search.
             </p>
-            <p className='project-desc text-accent'>
+            <p className='project-desc text-accent max-md:mb-16'>
               #openai #langchain #javascript
             </p>
           </div>
 
           <div className='section-column'>
             <p className='text-title2'>LastResort</p>
-            <img
-              loading='lazy'
-              src={img.lastresortLogo}
-              className='project-img'
-            />
+            <a
+              href='https://github.com/arrickx/lastResort'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                loading='lazy'
+                src={img.lastresortLogo}
+                alt='LastResort Logo'
+                className='project-img'
+              />
+            </a>
             <p className='project-desc'>
               A website where anonymous micro-stories capture the fleeting
               essence of daily life, inviting you to share, reflect, and
               connect.
             </p>
-            <p className='project-desc text-accent'>#react #express #webpack</p>
+            <p className='project-desc text-accent max-md:mb-16'>
+              #react #express #webpack
+            </p>
           </div>
         </section>
 
-        <footer>
+        <footer className='max-md:-mt-24'>
           <p className='text-lg md:text-xl lg:text-2xl text-accent'>
             Always coding, always learning.
           </p>
@@ -205,7 +221,7 @@ function App() {
               target='_blank'
               rel='noopener noreferrer'
             >
-              <img loading='lazy' src={img.githubLogo} />
+              <img loading='lazy' src={img.githubLogo} alt='GitHub Logo' />
             </a>
             <a
               className='footer-icon bg-zinc-900'
@@ -213,13 +229,14 @@ function App() {
               target='_blank'
               rel='noopener noreferrer'
             >
-              <img loading='lazy' src={img.linkedinLogo} />
+              <img loading='lazy' src={img.linkedinLogo} alt='LinkedIn Logo' />
             </a>
             <a
               className='footer-icon bg-zinc-900'
-              href='mailto:richterxlau@gmail.com?subject=Reaching%20out%20from%20%5BCompany%20Name%5D&body=Hi%20Richter,%0D%0AThis%20is%20%5BName%5D%20from%20%5BCompany%20Name%5D.%20Are%20you%20interested%20in%20learning%20more%20about%20potential%20roles%20at%20our%20company%3F%20I%27d%20be%20happy%20to%20share%20some%20information%20and%20answer%20any%20questions%20you%20have.%0D%0ABest,%0D%0A%5BName%5D'
+              href='mailto:richterxlau@gmail.com?subject=Reaching%20out%20from%20%5BCompany%20Name%5D&body=Hi%20Richter,%0D%0A%0D%0AThis%20is%20%5BName%5D%20from%20%5BCompany%20Name%5D.%20Are%20you%20interested%20in%20learning%20more%20about%20potential%20roles%20at%20our%20company%3F%20I%27d%20be%20happy%20to%20share%20some%20information%20and%20answer%20any%20questions%20you%20have.%0D%0A%0D%0ABest,%0D%0A%5BName%5D'
+              rel='noopener noreferrer'
             >
-              <img loading='lazy' src={img.emailLogo} />
+              <img loading='lazy' src={img.emailLogo} alt='Email Logo' />
             </a>
           </div>
         </footer>
